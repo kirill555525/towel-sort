10f80d4005f01d222538807da5bdd82f887f856a
kirill555525 <surskillwill@gmail.com>
2024-08-20 03:38:41 +0500

    feat: complete task

--- src/index.js
+++ src/index.js
@@ -2,5 +2,12 @@
 // You should implement your task here.
 
 module.exports = function towelSort (matrix) {
-  return [];
+  const lst = [];
+  if (typeof(matrix) !== 'object') return lst;
+  for (let i = 0; i < matrix.length; i++) {
+    if (i % 2 == 0) lst.push(...matrix[i]);
+    else lst.push(...matrix[i].reverse());
+  }
+
+  return lst;
 }
